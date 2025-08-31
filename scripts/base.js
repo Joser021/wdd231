8946b44f50cb93a683ffda77fd4b543415ac62ec
joser021 <joser.dev021@gmail.com>
2025-08-31 20:28:54 -0300

    updating student photo, improving hamburger menu

--- scripts/base.js
+++ scripts/base.js
@@ -17,3 +17,24 @@ document.querySelectorAll(".nav-links a").forEach(n => n.addEventListener("click
     hamburger.classList.remove("active");
     navLinks.classList.remove("active");
 }));
+
+document.addEventListener("click", (c) => {
+    if (!hamburger.contains(c.target) && !navLinks.contains(c.target)) {
+        navLinks.classList.remove("active");
+        hamburger.classList.remove("active");
+    }
+})
+
+let touchStart = 0;
+document.addEventListener("touchstart", (t) => {
+    touchStart = t.touches[0].clientX;
+});
+
+document.addEventListener("touchend", (t) => {
+    const touchEnd = t.changedTouches[0].clientX;
+
+    if (touchStart !== touchEnd && !hamburger.contains(t.target) && !navLinks.contains(t.target)) {
+        navLinks.classList.remove("active")
+        hamburger.classList.remove("active")
+    }
+})
