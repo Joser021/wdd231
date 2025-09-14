a909d39261a87784a386e563942350a6b63e5835
joser021 <joser.dev021@gmail.com>
2025-09-14 18:37:21 -0300

    responsive

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -5,6 +5,40 @@ const lastModified = document.querySelector("#lastModified");
 year.innerHTML = today.getFullYear();
 lastModified.innerHTML = document.lastModified;
 
+const hamburger = document.querySelector("#ham-menu");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+    hamburger.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-links a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+}))
+
+document.addEventListener("click", (c) => {
+    if (!hamburger.contains(c.target) && !navLinks.contains(c.target)){
+        navLinks.classList.remove("active");
+        hamburger.classList.remove("active");
+    }
+})
+
+let touchStart = 0;
+document.addEventListener("touchstart", (t) => {
+    touchStart = t.touches[0].clientX;
+})
+
+document.addEventListener("touchend", (t) => {
+    const touchEnd = t.changedTouches[0].clientX;
+
+    if (touchStart !== touchEnd && !hamburger.contains(t.target) && !navLinks.contains(t.target)) {
+        navLinks.classList.remove("active");
+        hamburger.classList.remove("active");
+    }
+})
+
 const url = "scripts/members.json";
 const cards = document.querySelector(".cards");
 
