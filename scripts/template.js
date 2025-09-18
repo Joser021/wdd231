fc5597e043041ddddb6680a9f40c3d8f4ad9d587
joser021 <joser.dev021@gmail.com>
2025-09-18 16:31:09 -0300

    API, templates

--- scripts/template.js
+++ scripts/template.js
@@ -1,15 +1,17 @@
+const town = document.querySelector("#town");
+const description = document.querySelector("#description");
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
-const captionDesc = document.querySelector("figcaption");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=5ae73727814ab3e86c467c8a3bd3ee63"
+const url = "//api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&units=imperial&appid=5ae73727814ab3e86c467c8a3bd3ee63"
 
 async function apiFetch() {
-    try {
+    try {   
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -18,4 +20,15 @@ async function apiFetch() {
     }
 }
 
-// apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}`;
+    console.log("hello");
+    town.innerHTML = data.name;
+    description.innerHTML = data.weather[0].description;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    weatherIcon.setAttribute("src", iconsrc)
+    weatherIcon.setAttribute("alt", data.weather[0].description)
+}
+
+apiFetch();
