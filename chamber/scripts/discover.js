6ef9e83ca19b248d4f7b365cdec248f7d4aea7fe
joser021 <joser.dev021@gmail.com>
2025-10-04 17:05:27 -0300

    dialogbox

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,17 +1,15 @@
 import { places } from "../data/places.mjs"
-console.log(places);
-
 
 const discorverCard = document.querySelector(".discover-card");
 
 places.forEach((place) => {
-    // console.log(place.photoUrl);
     const card = document.createElement("section");
     const name = document.createElement("h2");
     const description = document.createElement("p");
     const address = document.createElement("p");
     const cost = document.createElement("p");
     const image = document.createElement("img");
+    const button = document.createElement("button");
 
     image.setAttribute("src", `${place.photoUrl}`);
     image.setAttribute("alt", `Picture of ${place.name}`);
@@ -21,15 +19,44 @@ places.forEach((place) => {
     description.textContent = `${place.description}`;
     address.textContent = `${place.address}`;
     cost.textContent = `${place.cost}`;
+    button.textContent = "Learn more"
+    
 
     card.appendChild(image)
     card.appendChild(name)
-    // card.appendChild(cost)
     card.appendChild(description)
     card.appendChild(address)
+    card.appendChild(button)
 
     discorverCard.appendChild(card)
-    
-//     console.log(place.name);
+
+    button.addEventListener("click", () => {
+        displayPlaceCost(place)
+    })
 })
 
+
+// DIALOG BOX
+const placeCost = document.querySelector("#place-cost");
+
+function displayPlaceCost(place) {
+    placeCost.innerHTML = ``;
+    placeCost.innerHTML = `
+    <h2>${place.name} Membership</h2>
+    <h3>${place.cost}</h3>
+    <button id="closeModal">close</button>`;
+
+
+    placeCost.showModal();
+
+    const closeModal = document.querySelector("#closeModal");
+    closeModal.addEventListener("click", () => {
+        placeCost.close();
+    })
+
+    placeCost.addEventListener("click", (c) => {
+        if (c.target === placeCost) {
+            placeCost.close();
+        }
+    });
+}
